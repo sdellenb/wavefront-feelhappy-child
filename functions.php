3525a140cc4dbf24af6d880ef16565975db9448c
Simon Dellenbach <simon.dellenbach@autodesk.com>
2015-11-18 13:38:42 +0100

    Force the correct loading order of child/parent theme style.css.
    With wavefront (from back in 2009), they are added to the header in the wrong order, resulting in not being able
    to override styles in the child theme.

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php
 
 function theme_enqueue_styles() {
+	// "Your child theme's stylesheet will usually be loaded automatically.
+	//  If it is not, you will need to enqueue it as well. Setting 'parent-style' as
+	//  a dependency will ensure that the child theme stylesheet loads after it."
+	// See https://codex.wordpress.org/Child_Themes
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style )
+	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
