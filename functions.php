5f26622f5c578994e96034bc10c1098d4922c6f4
Simon Dellenbach <simon.dellenbach@autodesk.com>
2015-11-24 13:03:59 +0100

    Remove custom wp_login_failed handler, as wp-cerber is taking care of that now.

--- functions.php
+++ functions.php
@@ -17,16 +17,3 @@ function favicon_link() {
 	echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'favicon_link' );
-
-/**
- * Log login failures with SYSLOG to avoid monitoring the huge access.log.
- * credit to http://wireflare.com/wordpress-login-security-fail2ban/
- */
-const SYSLOG_FACILITY = LOG_LOCAL1;
-
-add_action('wp_login_failed', 'log_failed_attempt');
-
-function log_failed_attempt( $username ) {
-	openlog( 'wordpress('.$_SERVER['HTTP_HOST'].')', LOG_NDELAY|LOG_PID, SYSLOG_FACILITY);
-	syslog( LOG_NOTICE, "Wordpress authentication failure for $username from {$_SERVER['REMOTE_ADDR']}" );
-}
